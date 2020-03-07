876d428483fa0fe2947785e8c66cedcde332da0a
Marcelo Sampaio <marcelosampaio1965@gmail.com>
2020-03-07 14:06:27 -0300

    working on data entry validation 🍉

--- PondSim/Controller/DetailController.swift
+++ PondSim/Controller/DetailController.swift
@@ -17,20 +17,44 @@ class DetailController: UITableViewController {
     @IBOutlet weak var pesoTextField: UITextField!
     @IBOutlet weak var notaTextField: UITextField!
     
+    @IBOutlet weak var saveButton: UIButton!
+    @IBOutlet weak var deleteButton: UIButton!
+    
+    
     
     // MARK: - View Life Cycle
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        if item.prova.isEmpty {
+            deleteButton.isHidden = true
+        }else{
+            deleteButton.isHidden = false
+        }
     }
 
     // MARK: - UI Actions
     @IBAction func saveItem(_ sender: Any) {
         print("🥁 save item")
+        if !isValidDataEntry() {
+            print("🚣‍♂️ invalid data entry!!!")
+        }
     }
     
     @IBAction func deleteItem(_ sender: Any) {
         print("🥁 delete item")
     }
+    
+    // MARK: - Data Entry Helper
+    private func isValidDataEntry() -> Bool {
+        if pesoTextField.text!.isEmpty {
+            return false
+        }
+        if notaTextField.text!.isEmpty {
+            return false
+        }
+        return true
+    }
+    
+    
     
 }
